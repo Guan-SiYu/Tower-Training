8dc9f82fc79cfd1ac3b8037c81d7ad480aa048f3
guansiyu <guansiyu666@yeah.net>
2020-10-14 18:03:15 +0800

    修改:改动了test/app.test.js，但是router.js中的app.send改成了app.json()

--- router.js
+++ router.js
@@ -4,15 +4,13 @@ const router = express.Router()
 let flag = 0
 router.get('/start', function (req, res) {
     flag = Math.floor(Math.random() * 100 + 1)
-	// console.log(`生成的随机数为 : ${flag}`)
-	res.send('ok')
+	console.log(`生成的随机数为 : ${flag}`)
+	res.json({message:'ok',flag:flag})
 })
 router.get('/:number', function (req, res) {
 	const num = req.params.number;
-	if(num==flag)res.send('equal')
+	if(num==flag)res.json({message:'equal'})
 	console.log(res.body)
-    res.send(
-        num > flag ? 'bigger' : 'smaller'
-    )
+    res.json({message: num > flag ? 'bigger' : 'smaller'})
 })
 module.exports = router
--- test/app.test.js
+++ test/app.test.js
@@ -2,31 +2,27 @@ const app = require('../app')
 const supertest = require('supertest')
 const request = supertest(app)
 
-
-it('应返回ok',async done=>{
-	const response = await request.get('/start')
-	.send('ok')
-	expect(response.status).toBe(200)
-	done()
-})
-
 describe('判断大小',()=>{
+	let flag; 
+	it('应返回ok',async done=>{
+		const response = await request.get('/start')
+		flag = response.body.flag //访问/start同时把flag记录下来
+		expect(response.body.message).toBe('ok')
+		done()
+	})
 	it('返回smaller',async done=>{
-		const response = await request.get('/14')
-		.send('smaller')
-		expect(response.status).toBe(200)
+		const response = await (await request.get(`/${flag-1}`))
+		expect(response.body.message).toBe('smaller')
 		done()
 	})
 	it('返回bigger',async done=>{
-		const response = await request.get('/16')
-		.send('bigger')
-		expect(response.status).toBe(200)
+		const response = await request.get(`/${flag+1}`)
+		expect(response.body.message).toBe('bigger')
 		done()
 	})
 	it('返回equal',async done=>{
-		const response = await request.get('/15')
-		.send('equal')
-		expect(response.status).toBe(200)
+		const response = await request.get(`/${flag}`)
+		expect(response.body.message).toBe('equal')
 		done()
 	})
 })
