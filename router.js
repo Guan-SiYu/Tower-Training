19f4fc4beb7f116b65d7b8bd7e38e6cb630ce881
guansiyu <guansiyu666@yeah.net>
2020-10-15 16:59:04 +0800

    🍋单元测试3:完整玩一遍游 戏，回复equal时结束

--- router.js
+++ router.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-let flag = {randomNumber:0}
+let flag ;
 router.get('/start', function (req, res) {
-    flag.randomNumber = Math.floor(Math.random() * 100 + 1)
+    flag = Math.floor(Math.random() * 100 + 1)
 	res.send('ok')
 })
 router.get('/:number', function (req, res) {
 	const num = Number(req.params.number)
-	if(num === flag.randomNumber) return res.send('equal')
-    res.send( num > flag.randomNumber ? 'bigger' : 'smaller')
+	if(num === flag) return res.send('equal')
+    res.send( num > flag ? 'bigger' : 'smaller')
 })
-module.exports = {router,flag}
+module.exports = {
+	router,
+	getFlag:()=>{
+		return new Promise((resolve,reject)=>{
+			resolve(flag)
+		})
+	}
+}
--- test/app.test.js
+++ test/app.test.js
@@ -3,26 +3,28 @@ const supertest = require('supertest')
 const Router = require('../router')
 const request = supertest(app)
 
-describe('判断大小',()=>{
-	let flag = Router.flag //访问/start同时把flag记录下来
-	it('应返回ok',async  done=>{
+describe('完整玩一遍游戏',()=>{
+	let flag  //访问/start同时把flag记录下来
+	it('开始游戏',async  done=>{
 		const response = await request.get('/start')
+		flag = await Router.getFlag() //获取服务器生成的随机数
 		expect(response.text).toBe('ok')
 		done()
 	})
-	it('返回smaller',async done=>{
-		const response = await (await request.get(`/${flag.randomNumber-1}`))
-		expect(response.text).toBe('smaller')
-		done()
-	})
-	it('返回bigger',async done=>{
-		const response = await request.get(`/${flag.randomNumber+1}`)
-		expect(response.text).toBe('bigger')
-		done()
-	})
-	it('返回equal',async done=>{
-		const response = await request.get(`/${flag.randomNumber}`)
-		expect(response.text).toBe('equal')
-		done()
+	it('服务器回复equal结束',done=>{
+		async function toFind (left,right) {
+			mid = Math.floor((left+right)/2)
+			const response = await request.get(`/${mid}`)
+			const responseText = response.text
+			if(responseText === 'equal'){
+				expect(responseText).toBe('equal')
+				return done()
+			}else if(responseText === 'smaller' ){ //目标在左边
+				toFind(mid,right)
+			}else if(responseText === 'bigger'){ //目标在右边
+				toFind(left,mid)
+			} 
+		}
+		toFind(0,100)
 	})
 })
