0dcbfb5180b240b2b3f7ef065c7a684f7fc6e6ec
guansiyu <guansiyu666@yeah.net>
2020-10-19 14:46:30 +0800

    modified：递归直到调用callback & 递归返回一个promise对象

--- asyncExercise/async.js
+++ asyncExercise/async.js
@@ -2,77 +2,70 @@ const rp = require('request-promise')
 const request = require('request')
 
 /* --------------------- CallBack ---------------------- */
-// start
-request(`http://localhost:3000/start`,(error, response, body)=> {
-	try{
-		if((response && response.statusCode === 200))console.log(`Promise方法开始游戏 *${body}`)
-	}catch {
-		return console.error('error:', error)
-	}
-  })
-// play:
-function toFind_cb(left,right) {
+
+function toFind_cb(left, right, CallBack) {
 	const mid = Math.floor((left + right) / 2)
-	request(`http://localhost:3000/${mid}`, (error, response, body)=> {
-	try{
-		if(response && response.statusCode === 200){
-			console.log('body:', body)
-			if (body === 'equal') {
-				return console.log(`猜中数字${mid}`)
-			} else if (body === 'smaller') { 
-				toFind_cb(mid, right)
-			} else if(body === 'bigger') { 
-				toFind_cb(left, mid)
+	request(`http://localhost:3000/${mid}`, (error, response, body) => {
+		try {
+			if (response && response.statusCode === 200) {
+				if (body === 'equal') {
+					CallBack(mid)
+				} if (body === 'smaller') {
+					toFind_cb(mid, right, CallBack)
+				} else if (body === 'bigger') {
+					toFind_cb(left, mid, CallBack)
+				}
 			}
+		} catch {
+			return console.error('error:', error)
 		}
-	}catch{
-		return console.error('error:', error)
-	}
-  })
+	})
 }
-// 调用:
-// toFind_cb(0,100)
+// play the game:
+request('http://localhost:3000/start', (error, response) => {
+	try {
+		if ((response && response.statusCode === 200)) {
+			toFind_cb(0, 101, (foundNum) => console.log(`The foundNum is ${foundNum}`))
+		}
+	} catch {
+		console.error('error:', error)
+	}
+})
 
 /* --------------------- Promise ---------------------- */
-// start:
-rp(`http://localhost:3000/start`)
-.then(responseText=>console.log(`Promise方法开始游戏 *${responseText}`))
-// Play:
+
 function toFind_pm(left, right) {
 	const mid = Math.floor((left + right) / 2)
-	rp(`http://localhost:3000/${mid}`)
-    	.then((responseText)=>{
-			console.log('mid = '+ mid)
-			console.log(responseText)
+	return rp(`http://localhost:3000/${mid}`)
+		.then((responseText) => {
 			if (responseText === 'equal') {
-				return console.log(`猜中数字${mid}`)
-			} else if (responseText === 'smaller') { 
-				toFind_pm(mid, right)
-			} else if(responseText === 'bigger') { 
+				return mid
+			} if (responseText === 'smaller') {
+				return toFind_pm(mid, right)
+			} if (responseText === 'bigger') {
+				return toFind_pm(left, mid) // return a promiseObj
 			}
 		})
-    	.catch((err)=>console.log(err))
+		.catch((err) => console.log(err))
 }
-// 调用:
-// toFind_pm(1,100)
+// play the game:
+rp('http://localhost:3000/start')
+	.then(() => toFind_pm(1, 101)).then((foundNum) => console.log(`The foundNum is ${foundNum}`))
 
 /* --------------------- Async & Await ---------------------- */
-// start:
-rp(`http://localhost:3000/start`)
-.then(responseText=>console.log(`Async & Await方法开始游戏 *${responseText}`))
 
 async function toFind_await(left, right) {
 	const mid = Math.floor((left + right) / 2)
 	const responseText = await rp(`http://localhost:3000/${mid}`)
-	console.log('mid = '+ mid)
 	if (responseText === 'equal') {
-		return console.log(`猜中数字${mid}`)
-	} else if (responseText === 'smaller') { 
-		await toFind_await(mid, right)
-	} else if (responseText === 'bigger') { 
-		await toFind_await(left, mid)
+		return mid // return a promiseObj(<value> = foundNum)
+	} if (responseText === 'smaller') {
+		return toFind_await(mid, right)
+	} if (responseText === 'bigger') {
+		return toFind_await(left, mid)
 	}
 }
-//调用：
-toFind_await(1,101)
 
+// play the game:
+rp('http://localhost:3000/start')
+	.then(() => toFind_await(1, 101)).then((foundNum) => console.log(`The foundNum is ${foundNum}`))
